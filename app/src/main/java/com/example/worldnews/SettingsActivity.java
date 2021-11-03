335799ac91bfcf7c71510f84b4ef726b69693f6c
IRFAN-GujjAR <irfan.gujjar.business@gmail.com>
2021-11-04 02:19:54 +0500

    Updates

--- app/src/main/java/com/example/worldnews/SettingsActivity.java
+++ app/src/main/java/com/example/worldnews/SettingsActivity.java
@@ -1,8 +1,8 @@
 package com.example.worldnews;
 
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
--- app/src/main/java/com/example/worldnews/SettingsFragment.java
+++ app/src/main/java/com/example/worldnews/SettingsFragment.java
@@ -1,13 +1,11 @@
 package com.example.worldnews;
 
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceFragmentCompat;
-import android.support.v7.preference.PreferenceScreen;
-import android.widget.Toast;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
